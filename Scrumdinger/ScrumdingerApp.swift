a47cc22f8834c954a13a87ca8ae139e1d7d61a4c
Raja Suman <rajasuman09@gmail.com>
2024-02-04 12:34:09 +0530

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Raja Rama Dasa on 04/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
